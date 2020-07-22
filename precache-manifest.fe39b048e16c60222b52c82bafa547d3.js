d08c4a77703a7aa1f5a36d9421ae1cb24f48e5e5
ivan-piminov <ivan-piminov@yandex.ru>
2020-07-22 20:05:11 +0300

    Updates

---
+++ precache-manifest.fe39b048e16c60222b52c82bafa547d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96e3988dfff486b755f43b659a51748",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "c13a8e2ade6eb766b4a4",
+    "url": "/Counter/static/css/main.d4cd6703.chunk.css"
+  },
+  {
+    "revision": "e71ae8c17f540502f030",
+    "url": "/Counter/static/js/2.0d419dad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Counter/static/js/2.0d419dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13a8e2ade6eb766b4a4",
+    "url": "/Counter/static/js/main.c01a4b16.chunk.js"
+  },
+  {
+    "revision": "dcb86cfd00a1080aa0e5",
+    "url": "/Counter/static/js/runtime-main.a701aedb.js"
+  }
+]);
